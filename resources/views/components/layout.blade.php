91d562293fd71a6fcd9f16c5ae869863c124d401
ell-ska <ella@skagerstrom.se>
2025-01-28 23:08:16 +0100

    feat: create app layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" class="font-primary bg-slate-800 text-slate-50 antialiased">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="{{ asset('favicon.svg') }}" />
+        <title>{{ isset($title) ? "$title | Popco" : 'Popco' }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="flex min-h-screen flex-col items-center">
+        <main class="w-full max-w-5xl px-4">
+            {{ $slot }}
+        </main>
+    </body>
+</html>
