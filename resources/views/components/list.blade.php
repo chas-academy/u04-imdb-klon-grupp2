5cf7d824316a3a089f226e818fd6dc5e8b8d8058
ell-ska <ella@skagerstrom.se>
2025-01-30 16:58:48 +0100

    feat: list component

---
+++ resources/views/components/list.blade.php
@@ -0,0 +1,21 @@
+@props([
+    'title',
+    'posters',
+])
+
+@php
+    $posters = array_pad($posters, 4, ['src' => null, 'alt' => null]);
+@endphp
+
+<div {{ $attributes->merge(['class' => 'flex flex-col gap-2']) }}>
+    <div class="grid grid-cols-2 overflow-hidden rounded-xs">
+        @foreach ($posters as $poster)
+            <x-poster
+                :src="$poster['src']"
+                :alt="$poster['alt']"
+                :rounded="false"
+            />
+        @endforeach
+    </div>
+    <p class="font-bold">{{ $title }}</p>
+</div>
