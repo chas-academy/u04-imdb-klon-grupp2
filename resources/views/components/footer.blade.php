66f46527f650d55cdd37fc2193988d84be3a1085
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-01-30 15:20:20 +0100

    fix: remove unescessary file

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,5 @@
-<div>
-    <footer class="bg-slate-800 py-2 text-xs leading-4 text-indigo-400">
-        <div class="container mx-auto text-center">
-            <p>&copy; {{ date('Y') }} Made by Grupp 2</p>
-        </div>
-    </footer>
-</div>
+<footer class="bg-slate-800 py-2 text-xs leading-4 text-indigo-400">
+    <div class="container mx-auto text-center">
+        <p>&copy; {{ date('Y') }} Made by Grupp 2</p>
+    </div>
+</footer>
