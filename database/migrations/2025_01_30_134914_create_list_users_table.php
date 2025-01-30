5b93cfdf96c338df9e21f3d52b37577c7c00fc3c
Albin Wrebo <78490851+beforeAW@users.noreply.github.com>
2025-01-30 15:32:26 +0100

    Feat: migration created

---
+++ database/migrations/2025_01_30_134914_create_list_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\MovieList;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('list_users', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', ['pending', 'accepted']);
+            $table->enum('role', ['owner', 'collaborator']);
+            $table->foreignIdFor(MovieList::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(User::class, 'user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('list_users');
+    }
+};
