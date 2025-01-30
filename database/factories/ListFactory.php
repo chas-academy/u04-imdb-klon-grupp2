c2a4145e5e11881647babdd01ef5e2d7b13ef39d
David Dyberg <david.dyberg@gmail.com>
2025-01-30 18:39:04 +0100

    fix: formatting

--- database/factories/ListFactory.php
+++ database/factories/ListFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\MovieList;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\MovieList>
@@ -11,6 +11,7 @@ use App\Models\MovieList;
 class ListFactory extends Factory
 {
     protected $model = MovieList::class;
+
     /**
      * Define the model's default state.
      *
