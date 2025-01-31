798f53a6204056bdeedf1357e5253febadbda60b
ell-ska <ella@skagerstrom.se>
2025-01-31 11:48:37 +0100

    feat: add screen reader label for icon buttons

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,6 +1,10 @@
-@props(['variant' => 'primary', 'size' => 'md', 'type' => 'button'])
+@props(['variant' => 'primary', 'size' => 'md', 'type' => 'button', 'srLabel' => null])
 
 @php
+    if ($variant === 'icon' && ! isset($srLabel)) {
+        throw new Exception('an icon button must have a screen reader label');
+    }
+
     $classes = [
         'rounded-full bg-indigo-400 text-slate-50 hover:bg-indigo-500' => $variant === 'primary',
         'rounded-full border-2 border-slate-100 text-slate-100 hover:border-indigo-400 hover:text-indigo-400' => $variant === 'secondary',
@@ -13,8 +17,22 @@
 
 @if ($type === 'button')
     <button {{ $attributes->class($classes) }}>
+        @isset($srLabel)
+            <span class="sr-only">
+                {{ $srLabel }}
+            </span>
+        @endisset
+
         {{ $slot }}
     </button>
 @elseif ($type === 'link')
-    <a {{ $attributes->class($classes) }}>{{ $slot }}</a>
+    <a {{ $attributes->class($classes) }}>
+        @isset($srLabel)
+            <span class="sr-only">
+                {{ $srLabel }}
+            </span>
+        @endisset
+
+        {{ $slot }}
+    </a>
 @endif
