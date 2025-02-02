2ea28b83e498a5b01e185e4bb12a7e12b047bac6
ell-ska <ella@skagerstrom.se>
2025-02-02 17:05:51 +0100

    feat: create movie component

---
+++ resources/views/components/movie.blade.php
@@ -0,0 +1,17 @@
+@props([
+    'title',
+    'rating',
+    'image',
+    'link',
+])
+
+<a
+    href="{{ $link }}"
+    {{ $attributes->merge(['class' => 'flex cursor-pointer flex-col gap-2 transition hover:scale-102']) }}
+>
+    <x-poster :src="$image" alt="Poster for {{ $title }}" />
+    <div class="flex flex-col gap-0.5">
+        <p class="line-clamp-3 text-base/snug font-bold">{{ $title }}</p>
+        <x-rating :rating="$rating" size="sm" />
+    </div>
+</a>
