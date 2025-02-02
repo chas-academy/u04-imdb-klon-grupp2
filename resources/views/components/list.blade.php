5d51e5d0eb719b5b21d92db44e5ee9a3892e4efa
ell-ska <ella@skagerstrom.se>
2025-02-02 16:59:47 +0100

    fix: add link to list component

--- resources/views/components/list.blade.php
+++ resources/views/components/list.blade.php
@@ -1,19 +1,20 @@
 @props([
     'title',
     'posters',
+    'link',
 ])
 
 @php
-    $posters = array_pad($posters, 4, ['src' => null, 'alt' => null]);
+    $posters = array_pad($posters, 4, ['src' => null, 'title' => null]);
     $posters = array_slice($posters, 0, 4);
 @endphp
 
-<div {{ $attributes->merge(['class' => 'flex flex-col gap-2']) }}>
+<a href="{{ $link }}" {{ $attributes->merge(['class' => 'flex cursor-pointer flex-col gap-2 transition hover:scale-102']) }}>
     <div class="grid grid-cols-2 overflow-hidden rounded-xs">
         @foreach ($posters as $poster)
             <x-poster
                 :src="$poster['src']"
-                :alt="$poster['alt']"
+                alt="Poster for {{ $poster['title'] }}"
                 :rounded="false"
             />
         @endforeach
