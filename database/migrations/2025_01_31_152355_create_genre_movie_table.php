451f7040a7005514fdf09fdd100a32de264ea5f7
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-03 16:24:10 +0100

    fix: remove duplicate statement in MovieList.php

--- database/migrations/2025_01_31_152355_create_genre_movie_table.php
+++ database/migrations/2025_01_31_152355_create_genre_movie_table.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Models\Movie;
 use App\Models\Genre;
+use App\Models\Movie;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
