9f9511867eb68039d12bcd9d27ad8ff03ed87bc7
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-03 16:13:39 +0100

    feat: add HasFactory trait to Movie and MovieList models

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Movie extends Model
 {
+
+    use HasFactory;
+
     public function lists(): BelongsToMany
     {
         return $this->belongsToMany(MovieList::class, 'list_movie', 'movie_id', 'list_id')
