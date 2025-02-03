fc6c2f9b222cbd3fee3e3c1d39dcc4d6cadd5b53
Albin Wrebo <78490851+beforeAW@users.noreply.github.com>
2025-02-03 21:08:45 +0100

    feat: add admins route and view

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,10 @@ Route::get('/list/{id}', function () {
     return view('movie-list');
 });
 
+Route::get('/admin', function () {
+    return view('admin');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -53,4 +57,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
