ae9608246c511ea28fb20f4665982a08049a69e6
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-03 15:22:35 +0100

    fix: remove unused use statment

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -2,19 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Movie extends Model
 {
-<<<<<<< HEAD
-    use HasFactory;
-
-    public function lists(): HasMany
-=======
     public function lists(): BelongsToMany
->>>>>>> 8c03deb (fix: add composite key to list_movie table)
     {
         return $this->belongsToMany(MovieList::class, 'list_movie', 'movie_id', 'list_id');
     }
