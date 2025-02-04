94fc1f92e7d3c0f6106a442e08db8c7ead983a6c
ell-ska <ella@skagerstrom.se>
2025-02-04 14:57:50 +0100

    feat: open and close modal

--- resources/views/components/modal/base.blade.php
+++ resources/views/components/modal/base.blade.php
@@ -1,4 +1,42 @@
-<div class="modal fixed inset-0 flex justify-center overflow-y-scroll">
-    <div class="absolute inset-0 bg-slate-800/80 backdrop-blur-sm"></div>
+@props([
+    'name',
+    'show' => false,
+])
+
+<div
+    x-data="{
+        show: @js($show),
+    }"
+    x-show="show"
+    x-on:open-modal.window="
+        if ($event.detail === '{{ $name }}') {
+            show = true
+        }
+    "
+    x-on:close-modal.window="
+        if ($event.detail === '{{ $name }}') {
+            show = false
+        }
+    "
+    x-init="
+        $watch('show', (show) => {
+            if (show) {
+                document.body.classList.add('overflow-hidden')
+            } else {
+                document.body.classList.remove('overflow-hidden')
+            }
+        })
+    "
+    x-cloak
+    @class([
+        'flex' => $show,
+        'hidden' => "$show",
+        'fixed inset-0 flex justify-center overflow-y-scroll',
+    ])
+>
+    <div
+        @click="show = false"
+        class="absolute inset-0 bg-slate-800/80 backdrop-blur-sm"
+    ></div>
     {{ $slot }}
 </div>
