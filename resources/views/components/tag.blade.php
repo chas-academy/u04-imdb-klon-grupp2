ac6173bfbcac56213b50c039f4f261a6aaa0f541
ell-ska <ella@skagerstrom.se>
2025-02-04 14:41:54 +0100

    fix: remove tag

--- resources/views/components/tag.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    class="inline-block rounded-full px-4 py-2 font-bold text-slate-100
-    {{ $size === 'small' ? 'text-sm' : 'text-md' }}
-    {{ $hoverState ? "bg-slate-700 text-slate-100 hover:bg-slate-600" : }}
-    transition-colors duration-200" >
-
-    {{ $label }}
-</div>
