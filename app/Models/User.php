46869ff56b864d3a82e7f00925b9782d6dd0e185
Jelena Blagojevic <blagojevic_jelen@hotmail.com>
2025-02-04 14:25:01 +0100

    friends pivot table

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -46,11 +47,29 @@ class User extends Authenticatable
         ];
     }
 
-    // Many-to-many relationship with MovieList
     public function lists()
     {
         return $this->belongsToMany(MovieList::class, 'list_users', 'user_id', 'list_id')  // Explicitly defining the foreign key and related key
             ->withPivot('status', 'role') // Include pivot data
             ->withTimestamps(); // Automatically manage created_at and updated_at
+    }
+
+    protected function friendsOfMine(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'friends', 'user_id', 'friend_id')
+            ->wherePivot('status', 'accepted')
+            ->withTimestamps();
+    }
+
+    protected function friendsOf(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'friends', 'friend_id', 'user_id')
+            ->wherePivot('status', 'accepted')
+            ->withTimestamps();
+    }
+
+    public function friends()
+    {
+        return $this->friendsOfMine->merge($this->friendsOf);
     }
 }
