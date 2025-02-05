a70d2e640e93b3844ef6bce2e69e7f531ab58982
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-05 03:46:51 +0100

    fix: add has factory to review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,10 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Review extends Model
 {
-    public function user()
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'content',
+        'rating',
+        'movie_id',
+        'user_id',
+    ];
+
+    /**
+     * Get the user who wrote the review.
+     */
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
