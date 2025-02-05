9f775de0977f4b22d4ec7282e166a1e18c942674
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-05 04:53:16 +0100

    feat: Define the Seeder

---
+++ database/seeders/ReportSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Report;
+use Illuminate\Database\Seeder;
+
+class ReportSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create 20 reports
+        Report::factory()->count(20)->create();
+    }
+}
