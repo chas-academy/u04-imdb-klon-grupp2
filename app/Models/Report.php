c4d78adb8e5ccf2009c7e37acf582349e81eefaf
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-05 04:24:17 +0100

    feat: Update the Report Model

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Report extends Model
 {
-    //
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function review(): BelongsTo
+    {
+        return $this->belongsTo(Review::class);
+    }
 }
