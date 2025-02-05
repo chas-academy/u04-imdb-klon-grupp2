0ca5e457a54549d18194617c0cc21d91c034088b
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-06 00:00:04 +0100

    fix: update index.blade.php and serve it as an example file

--- resources/views/components/section-header/index.blade.php
+++ resources/views/components/section-header/index.blade.php
@@ -1,6 +1,18 @@
-@props([
-    'title',
-    'extraLabel' => null,
-    'href' => null,
-    'backLabel' => 'Back',
-])
+{{--
+    index.blade.php serves as an example file for how the components in section-head/ can be used.
+    
+    
+    <x-section-header.back-link
+    title="The Worst Person in The World"
+    href="#"
+    extraLabel="Review"
+    />
+    
+    <x-section-header.link
+    title="Best Movies 2025"
+    extraLabel="Featured"
+    href="#"
+    />
+    
+    <x-section-header.no-link title="Action" extraLabel="Public" />
+--}}
