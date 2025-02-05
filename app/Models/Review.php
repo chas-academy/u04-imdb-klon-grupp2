d45c0e240edfd47f3cbbc3212b37813fd6529e9b
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-05 02:22:17 +0100

    fix: add missing review relationship

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
+<<<<<<< HEAD
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -15,4 +17,18 @@ class Review extends Model
     {
         return $this->belongsTo(Movie::class);
     }
+=======
+    public function movie(): BelongsTo
+    {
+        return $this->belongsTo(Movie::class);
+    }
+
+    /**
+     * Get the user who wrote the review.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+>>>>>>> 0f88630 (fix: add missing review relationship)
 }
