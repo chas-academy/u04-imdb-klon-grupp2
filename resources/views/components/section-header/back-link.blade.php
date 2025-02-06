e33135786bb1518d9d12d2fcc22dcfc6c879a8c3
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-06 08:52:34 +0100

    feat: Add hyperlink functionality to extra label in section header component

--- resources/views/components/section-header/back-link.blade.php
+++ resources/views/components/section-header/back-link.blade.php
@@ -3,12 +3,13 @@
     'extraLabel' => null,
     'href' => null,
     'backLabel' => 'Back',
+    'extraHref' => null,
 ])
 
 <a href="{{ $href }}" class="group flex flex-col">
     <div class="flex items-center gap-2">
         <x-lucide-move-left
-            class="h-6 w-6 text-indigo-200 group-hover:text-indigo-300"
+            class="focus:text-indigo-00 h-6 w-6 text-indigo-200 group-hover:text-indigo-300"
         />
         <span class="text-base text-slate-200 group-hover:text-indigo-300">
             {{ $backLabel }}
@@ -19,5 +20,7 @@
     <h1 class="text-2xl font-bold text-slate-50">
         {{ $title }}
     </h1>
-    <span class="text-xs text-indigo-400">{{ $extraLabel }}</span>
+    <a href="{{ $extraHref }}">
+        <span class="text-xs text-indigo-400">{{ $extraLabel }}</span>
+    </a>
 </div>
