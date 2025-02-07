e5c923ef31334708f22d03e7c9f96534ff1564fe
ell-ska <ella@skagerstrom.se>
2025-02-07 16:37:46 +0100

    fix: section component bugs

--- app/utils/helpers.php
+++ app/utils/helpers.php
@@ -12,3 +12,18 @@ function formatDate($date)
         return $dateTime->format('d F Y');
     }
 }
+
+function validateBreakpoint(string $breakpoint)
+{
+    $validBreakpoints = ['base', 'sm', 'md', 'lg'];
+    if (! in_array($breakpoint, $validBreakpoints)) {
+        throw new Exception("$breakpoint is not a valid breakpoint");
+    }
+}
+
+function generateSectionGridVariable(string $breakpoint, int $number)
+{
+    validateBreakpoint($breakpoint);
+
+    return '--section-grid-cols-'.$breakpoint.': repeat('.$number.', minmax(0, 1fr))';
+}
