6da7c7baeb49a9ac9c3f94c97f20d2fcdc4b577d
ell-ska <ella@skagerstrom.se>
2025-02-08 17:39:57 +0100

    fix: extra spacing on scrollable list

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -21,21 +21,23 @@
     $styleAttributeValue = implode('; ', $twVariables);
 @endphp
 
-<section
-    style="{{ $styleAttributeValue }}"
-    @class([
-        'grid *:size-full' => ! $scrollableOnMobile,
-        'hide-scrollbar relative -mr-4 flex snap-x overflow-x-scroll *:snap-start *:not-[.gradient]:min-w-32 sm:mr-0 sm:grid sm:overflow-visible sm:*:size-full' => $scrollableOnMobile,
-        'sm:grid-cols-(--section-grid-cols-sm)' => isset($columns['sm']),
-        'md:grid-cols-(--section-grid-cols-md)' => isset($columns['md']),
-        'lg:grid-cols-(--section-grid-cols-lg)' => isset($columns['lg']),
-        'grid-cols-(--section-grid-cols-base) gap-4',
-    ])
->
-    {{ $slot }}
+<section class="relative">
+    <div
+        style="{{ $styleAttributeValue }}"
+        @class([
+            'grid *:size-full' => ! $scrollableOnMobile,
+            'hide-scrollbar -mr-4 flex snap-x overflow-x-scroll pr-4 *:snap-start *:not-[.gradient]:min-w-32 sm:mr-0 sm:grid sm:overflow-visible sm:*:size-full' => $scrollableOnMobile,
+            'sm:grid-cols-(--section-grid-cols-sm)' => isset($columns['sm']),
+            'md:grid-cols-(--section-grid-cols-md)' => isset($columns['md']),
+            'lg:grid-cols-(--section-grid-cols-lg)' => isset($columns['lg']),
+            'grid-cols-(--section-grid-cols-base) gap-4',
+        ])
+    >
+        {{ $slot }}
+    </div>
     @if ($scrollableOnMobile)
         <div
-            class="gradient pointer-events-none sticky -right-4 min-h-full min-w-12 bg-linear-to-l from-slate-800 to-slate-800/0 sm:hidden"
+            class="gradient pointer-events-none absolute inset-y-0 -right-4 min-w-12 bg-linear-to-l from-slate-800 to-slate-800/0 sm:hidden"
         ></div>
     @endif
 </section>
