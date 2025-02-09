bd8bd91972da23e9668a4af7f880c88003e23fb3
Jelena Blagojevic <blagojevic_jelen@hotmail.com>
2025-02-09 11:09:45 +0100

    making-it-responsive

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -2,9 +2,9 @@
     'label',
     'link'
     ])
-
 <a href="{{$link}}" class="transition ease-in-out duration-300 transform hover:scale-105">
-    <div class="ease-in-out bg-slate-700 hover:bg-slate-600 rounded-lg text-md md:text-sm text-white font-bold px-4 py-2 md:text-sm">   
-        <p>{{$label}}</p>
+    <div class="ease-in-out bg-slate-700 hover:bg-slate-600 rounded-lg text-white font-bold px-4 py-2 sm:px-2.5 sm:py-0.5 w-auto h-auto flex justify-center items-center">   
+        <p class="text-md sm:text-sm text-center">{{$label}}</p>
     </div>
+
 
