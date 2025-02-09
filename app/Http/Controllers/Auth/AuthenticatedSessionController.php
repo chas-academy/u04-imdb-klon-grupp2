1443d9842e378298e840939afc6b94e79744b9fa
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-09 07:01:50 +0100

    feat: add login feature

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
@@ -22,13 +23,17 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
+
     public function store(LoginRequest $request): RedirectResponse
     {
-        $request->authenticate();
 
-        $request->session()->regenerate();
+        if ($request->authenticate()) {
+            $request->session()->regenerate();
+            return redirect()->intended(route('home'));
+        }
 
-        return redirect()->intended(route('home', absolute: false));
+        // If authentication fails, return back with an error message
+        return back()->withErrors(['credentials'])->onlyInput('username');
     }
 
     /**
@@ -39,9 +44,8 @@ class AuthenticatedSessionController extends Controller
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect(route('home'));
     }
 }
