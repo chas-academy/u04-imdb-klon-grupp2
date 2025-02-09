158fd0828d2315c5cfd45833af737942e61922f8
David Dyberg <david.dyberg@gmail.com>
2025-02-09 14:43:36 +0100

    feat: add content on review view

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -1,3 +1,38 @@
+@php
+    $formattedDate = formatDate($review->created_at);
+    $backLabel = request('from') === 'movie' ? 'Back to movie' : 'Back to profile';
+    $backHref = url()->previous();
+@endphp
+
 <x-layout>
-    <h1>Review</h1>
+    <x-section-header.back-link
+        title="{{ $review->movie->title }}"
+        extraLabel="Review"
+        href="{{ $backHref }}"
+        backLabel="{{ $backLabel }}"
+    />
+    <div class="relative mt-3 flex items-center gap-4">
+        <x-poster class="w-32 sm:w-40" src="{{ $review->movie->poster}}" />
+        <x-lucide-ellipsis-vertical
+            class="absolute top-0 right-0 size-6 cursor-pointer text-slate-50 sm:-top-10"
+        />
+
+        <div class="flex w-full flex-col gap-3">
+            <x-rating size="lg" rating="{{ $review->movie->rating_average }}" />
+
+            <div
+                class="flex items-center justify-between sm:flex-col sm:items-start sm:gap-1"
+            >
+                <x-profile-simplified
+                    username="{{ $review->user->username }}"
+                    image="{{ $review->user->image }}"
+                    size="sm"
+                />
+                <p class="text-xs text-slate-400">{{ $formattedDate }}</p>
+            </div>
+        </div>
+    </div>
+    <p class="mt-6 max-w-xl text-base/snug text-slate-100">
+        {{ $review->content }}
+    </p>
 </x-layout>
