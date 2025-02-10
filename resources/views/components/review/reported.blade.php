a3e7530abfddafb33dc6bdd38fbce8b98ce8dd1f
ell-ska <ella@skagerstrom.se>
2025-02-10 22:38:09 +0100

    feat: reported review component

---
+++ resources/views/components/review/reported.blade.php
@@ -0,0 +1,30 @@
+@props([
+    'title',
+    'content',
+    'image',
+    'username',
+    'link',
+])
+
+<a
+    {{ $attributes->class('flex gap-3 rounded-sm bg-slate-700 p-2 transition hover:scale-101') }}
+    href="{{ $link }}"
+>
+    <x-poster :src="$image" class="w-20" alt="Poster for {{ $title }}" />
+
+    <div class="flex flex-1 flex-col gap-1">
+        <div class="flex-1 space-y-1">
+            <h2 class="text-lg font-bold text-slate-50">
+                {{ $title }}
+            </h2>
+            <p class="line-clamp-2 text-slate-300">{{ $content }}</p>
+        </div>
+
+        <div class="flex items-center justify-between">
+            <p class="text-base text-slate-50">
+                {{ $username }}
+            </p>
+            <x-button variant="secondary" size="sm">Check report</x-button>
+        </div>
+    </div>
+</a>
