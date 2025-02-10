2a97ff35a7de9d0d6cbd0245483fb63ca5a626d2
Albin Wrebo <78490851+beforeAW@users.noreply.github.com>
2025-02-10 09:30:37 +0100

    feat: textarea input

---
+++ resources/views/components/input/textarea.blade.php
@@ -0,0 +1,22 @@
+@props([
+    'name',
+    'label',
+    'placeholder',
+    'value' => null,
+])
+
+<div {{ $attributes->class('flex flex-col gap-1') }}>
+    <x-input.label for="{{ $name }}">
+        {{ $label }}
+    </x-input.label>
+    <textarea
+        id="{{ $name }}"
+        name="{{ $name }}"
+        placeholder="{{ $placeholder }}"
+        rows="4"
+        style="resize: none"
+        class="rounded-lg bg-slate-700 px-4 py-2 text-base text-slate-50 placeholder:text-slate-400"
+    >
+{{ $value }}</textarea
+    >
+</div>
