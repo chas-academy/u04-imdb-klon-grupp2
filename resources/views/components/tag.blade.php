39cb429823878b5c29799deaedfee506608d7f97
Jelena Blagojevic <blagojevic_jelen@hotmail.com>
2025-02-10 17:53:12 +0100

    adjusted-styling-and-responsivity

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,11 +1,7 @@
 @props([
     'label',
     'link'
-    ])
-<a href="{{$link}}" class="transition ease-in-out duration-300 transition hover:scale-105">
-    <div class="ease-in-out bg-slate-700 hover:bg-slate-600 rounded-[4px] text-white font-bold w-18.25 h-10 sm:w-12.75 sm:h-5  px-4 py-2 sm:px-2.5 sm:py-0.5 w-auto h-auto flex justify-center items-center">   
-        <p class="text-md sm:text-sm text-center">{{$label}}</p>
-    </div>
+])
+<a href="{{$link}}" class="transition hover:scale-105 bg-slate-700 hover:bg-slate-600 rounded text-slate-100 font-bold sm:px-4 sm:py-2 px-2.5 py-0.5 inline-flex justify-center items-center text-sm sm:text-base self-start">
+    {{$label}}
 </a>
-
-
