1bc4b93d06abc8b198e2bcae946265e8cdc5176d
ell-ska <ella@skagerstrom.se>
2025-02-10 10:40:08 +0100

    feat: add lists on profile view

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,8 +1,27 @@
 @php
-    $title = $isCurrentUserProfile ? 'My reviews' : "$username's reviews";
+    $listsTitle = $isCurrentUserProfile ? 'My lists' : "$username's lists";
+    $reviewsTitle = $isCurrentUserProfile ? 'My reviews' : "$username's reviews";
 @endphp
 
 <x-layout>
+    @if ($lists->isNotEmpty())
+        <div class="flex flex-col items-start gap-4">
+            <x-section-header.link
+                title="{!! $listsTitle !!}"
+                href="{{ route('lists', ['username' => $username]) }}"
+            />
+            <x-section :columns="[2, 'md' => 4]">
+                @foreach ($lists as $list)
+                    <x-list
+                        title="{!! $list['title'] !!}"
+                        :posters="$list['posters']->toArray()"
+                        link="{{ route('list', ['id' => $list['id']]) }}"
+                    />
+                @endforeach
+            </x-section>
+        </div>
+    @endif
+
     @if ($reviews->isNotEmpty())
         <div class="flex flex-col items-start gap-4">
             <x-section-header.link
