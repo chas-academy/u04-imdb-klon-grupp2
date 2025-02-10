fdc62823caab48978f8920e5be71cf2e54ff2134
ell-ska <ella@skagerstrom.se>
2025-02-10 23:13:14 +0100

    feat: create updateRating function in Review model

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -5,10 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Movie extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'director',
+        'year',
+        'duration',
+        'description',
+        'poster',
+        'cover_image',
+        'rating_average',
+        'rating_amount',
+    ];
 
     public function lists(): BelongsToMany
     {
@@ -20,5 +33,18 @@ class Movie extends Model
     {
         return $this->belongsToMany(Genre::class, 'genre_movie', 'movie_id', 'genre_id')
             ->withTimestamps();
+    }
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function updateRating()
+    {
+        $average = $this->reviews()->avg('rating') ?? 0;
+        $amount = $this->rating_amount + 1;
+
+        $this->update(['rating_average' => $average, 'rating_amount' => $amount]);
     }
 }
