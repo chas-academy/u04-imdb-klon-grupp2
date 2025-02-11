ea59d6f9d11f356a52b5597674488cf3528bf0d5
Albin Wrebo <78490851+beforeAW@users.noreply.github.com>
2025-02-11 11:55:15 +0100

    fix: remove file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,24 @@
 <x-layout>
-
-    <x-input.file class="w-80" name="test" label="picture" filePickedLabel="Recommended size 800 x 800 px" error=""></x-input>
+    <div class="flex flex-col gap-4">
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+            @foreach ($topRatedMovies->take(3) as $movie)
+                <a
+                    href="{{ route('movie', ['id' => $movie->id, 'title' => $movie->title]) }}"
+                    class="transition hover:scale-101 nth-[2]:hidden nth-[3]:hidden sm:nth-[2]:block md:nth-[3]:block"
+                >
+                    <x-poster src="{{ $movie->poster }}" class="size-full" />
+                </a>
+            @endforeach
+        </div>
+        <x-section :columns="[4, 'lg' => 6]" scrollableOnMobile>
+            @foreach ($topRatedMovies->skip(1) as $movie)
+                <a
+                    href="{{ route('movie', ['id' => $movie->id, 'title' => $movie->title]) }}"
+                    class="transition hover:scale-101 sm:nth-[1]:hidden sm:nth-[n+6]:hidden md:nth-[2]:hidden md:nth-[n+6]:block md:nth-[n+7]:hidden lg:nth-[n+7]:block lg:nth-[n+9]:hidden"
+                >
+                    <x-poster src="{{ $movie->poster }}" class="size-full" />
+                </a>
+            @endforeach
+        </x-section>
+    </div>
 </x-layout>
