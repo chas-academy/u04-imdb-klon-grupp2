abdf29dc0f9a86a8e92a2376bf48d7ca2d2788fe
Albin Wrebo <78490851+beforeAW@users.noreply.github.com>
2025-02-11 10:44:58 +0100

    feat:alpine version

--- resources/views/components/input/file.blade.php
+++ resources/views/components/input/file.blade.php
@@ -5,16 +5,63 @@
     'value' => null,
     'error',
 ])
-<div {{ $attributes->class('flex flex-col gap-1') }}>
+<div
+    {{ $attributes->class('flex flex-col gap-1') }}
+    x-data="{ fileName: '', showFileName: false }"
+>
     <x-input.label for="{{ $name }}">
         {{ $label }}
     </x-input.label>
-    <x-button size="sm" class="w-32">Click me</x-button>
+    <button
+        type="button"
+        id="file-upload-button"
+        class="cursor-pointer"
+        tabindex="0"
+        aria-label="Choose File"
+        @click="
+            console.log('Button clicked');
+            setTimeout(() => document.getElementById('file-upload').click(), 0);
+        "
+        @keydown.space.prevent.stop="
+            console.log('Space pressed');
+            setTimeout(() => document.getElementById('file-upload').click(), 0);
+        "
+        @keydown.enter.prevent.stop="
+            console.log('Enter pressed');
+            setTimeout(() => document.getElementById('file-upload').click(), 0);
+        "
+    >
+        <x-button size="sm" class="w-32">Choose File</x-button>
+    </button>
     <input
         type="file"
-        id="avatar"
-        name="avatar"
+        x-ref="fileInput"
+        id="file-upload"
+        name="{{ $name }}"
         accept="image/png, image/jpeg"
+        class="absolute h-0 w-0 opacity-0"
+        @change="
+        console.log('File selected');
+        if ($refs.fileInput.files.length > 0) {
+            fileName = $refs.fileInput.files[0].name;
+            showFileName = true;
+        }
+    "
     />
+
+    <div id="file-name" x-show="showFileName">
+        <x-button
+            variant="secondary"
+            size="sm"
+            class="w-32"
+            @click="
+                console.log('Change File clicked');
+                setTimeout(() => document.getElementById('file-upload').click(), 0);
+            "
+        >
+            Change File
+        </x-button>
+        <span id="file-display-name" x-text="fileName"></span>
+    </div>
     <x-input.error message="{{ $error }}" />
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,4 @@
 <x-layout>
-    <div class="flex flex-col gap-4">
-        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-            @foreach ($topRatedMovies->take(3) as $movie)
-                <a
-                    href="{{ route('movie', ['id' => $movie->id, 'title' => $movie->title]) }}"
-                    class="transition hover:scale-101 nth-[2]:hidden nth-[3]:hidden sm:nth-[2]:block md:nth-[3]:block"
-                >
-                    <x-poster src="{{ $movie->poster }}" class="size-full" />
-                </a>
-            @endforeach
-        </div>
-        <x-section :columns="[4, 'lg' => 6]" scrollableOnMobile>
-            @foreach ($topRatedMovies->skip(1) as $movie)
-                <a
-                    href="{{ route('movie', ['id' => $movie->id, 'title' => $movie->title]) }}"
-                    class="transition hover:scale-101 sm:nth-[1]:hidden sm:nth-[n+6]:hidden md:nth-[2]:hidden md:nth-[n+6]:block md:nth-[n+7]:hidden lg:nth-[n+7]:block lg:nth-[n+9]:hidden"
-                >
-                    <x-poster src="{{ $movie->poster }}" class="size-full" />
-                </a>
-            @endforeach
-        </x-section>
-    </div>
+
     <x-input.file class="w-80" name="test" label="picture" error=""></x-input>
 </x-layout>
