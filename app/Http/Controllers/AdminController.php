f975a0b1f052783e580be1ab14036b9e33fef9f8
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-12 23:26:55 +0100

    feat: fetch 10 latest movies from database

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use App\Models\User;
 
 class AdminController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $latestUploadedMovies = Movie::latest()->limit(10)->get();
+
+        return view('admin.dashboard', compact('latestUploadedMovies'));
     }
 
     public function users()
