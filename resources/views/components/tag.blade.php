4146d0b4ee5d1402110fad7b12fde6aa5319f565
Jelena Blagojevic <blagojevic_jelen@hotmail.com>
2025-02-12 10:10:35 +0100

    hover fix

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -2,6 +2,7 @@
     'label',
     'link'
 ])
-<a href="{{$link}}" class="transition hover:scale-105 bg-slate-700 hover:bg-slate-600 rounded text-slate-100 font-bold sm:px-4 sm:py-2 px-2.5 py-0.5 inline-flex justify-center items-center text-sm sm:text-base self-start">
-    {{$label}}
+
+<a href="{{ $link }}" class="transition bg-slate-700 hover:bg-slate-600 rounded text-slate-100 font-bold sm:px-4 sm:py-2 px-2.5 py-0.5 inline-flex justify-center items-center text-sm sm:text-base self-start">
+    {{ $label }}
 </a>
