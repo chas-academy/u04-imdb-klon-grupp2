79762e7fe71a1dd38b0e6d5d6275a1e043321681
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-12 22:07:53 +0100

    feat: style and render users route

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,3 +1,28 @@
 <x-layout>
-    <h1>All users</h1>
+    <div class="my-4">
+        <x-section-header.back-link
+            backLabel="Back to dashboard"
+            title="All users"
+            href="{{ route('admin.dashboard') }}"
+        />
+    </div>
+
+    <div class="space-between m-auto max-w-xl">
+        <div class="flex flex-col gap-4">
+            @forelse ($users as $user)
+                <div class="flex items-center justify-between">
+                    <x-profile-simplified
+                        :username="'@'. $user->username"
+                        :image="$user->image"
+                        size="md"
+                    />
+                    <x-button variant="secondary" size="sm" class="ml-auto">
+                        Edit
+                    </x-button>
+                </div>
+            @empty
+                
+            @endforelse
+        </div>
+    </div>
 </x-layout>
