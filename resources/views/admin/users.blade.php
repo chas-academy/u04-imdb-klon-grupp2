8071027002ffca9bac9c40d2ef8cd7a8138bd38e
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-12 22:28:04 +0100

    fix: improve tailwind classes

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -7,22 +7,20 @@
         />
     </div>
 
-    <div class="space-between m-auto max-w-xl">
-        <div class="flex flex-col gap-4">
-            @forelse ($users as $user)
-                <div class="flex items-center justify-between">
-                    <x-profile-simplified
-                        :username="'@'. $user->username"
-                        :image="$user->image"
-                        size="md"
-                    />
-                    <x-button variant="secondary" size="sm" class="ml-auto">
-                        Edit
-                    </x-button>
-                </div>
-            @empty
-                <x-empty-state content="No users found." />
-            @endforelse
-        </div>
+    <div class="m-auto max-w-xl space-y-4">
+        @forelse ($users as $user)
+            <div class="flex items-center justify-between">
+                <x-profile-simplified
+                    :username="'@'. $user->username"
+                    :image="$user->image"
+                    size="md"
+                />
+                <x-button variant="secondary" size="sm" class="ml-auto">
+                    Edit
+                </x-button>
+            </div>
+        @empty
+            <x-empty-state content="No users found." />
+        @endforelse
     </div>
 </x-layout>
