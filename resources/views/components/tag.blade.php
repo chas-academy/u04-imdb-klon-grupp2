a9968eb1b12157a795c119d9585634821a166f60
Jelena Blagojevic <blagojevic_jelen@hotmail.com>
2025-02-12 10:24:34 +0100

    accepting changes in tag and home file

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,8 +1,11 @@
 @props([
     'label',
-    'link'
+    'link',
 ])
 
-<a href="{{ $link }}" class="transition bg-slate-700 hover:bg-slate-600 rounded text-slate-100 font-bold sm:px-4 sm:py-2 px-2.5 py-0.5 inline-flex justify-center items-center text-sm sm:text-base self-start">
+<a
+    href="{{ $link }}"
+    class="inline-flex items-center justify-center self-start rounded bg-slate-700 px-2.5 py-0.5 text-sm font-bold text-slate-100 transition hover:bg-slate-600 sm:px-4 sm:py-2 sm:text-base"
+>
     {{ $label }}
 </a>
