cec6512fc776afdb91a59dd274c42ee0cda7a090
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-12 22:30:39 +0100

    fix: remove unused code

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,14 +9,12 @@ class AdminController extends Controller
 {
     public function dashboard()
     {
-        return view(
-            'admin.dashboard'
-        );
+        return view('admin.dashboard');
     }
 
     public function users()
     {
-        $users = User::orderBy('username', 'asc')->get();
-        return view('admin.users', ['users' => $users]);
+        $users = User::orderBy('username')->get();
+        return view('admin.users', compact('users'));
     }
 }
