5730c9644f75d34d6887ecef5058188fa19b4f2c
ell-ska <ella@skagerstrom.se>
2025-02-13 15:16:27 +0100

    feat: lists empty state

--- resources/views/lists.blade.php
+++ resources/views/lists.blade.php
@@ -2,46 +2,62 @@
     $title = $isCurrentUserProfile ? 'My lists' : "$username's lists";
 @endphp
 
-<x-layout>
-    <div class="mt-1 space-y-6 md:mt-12">
-        <div class="flex items-start justify-between">
-            <x-section-header.back-link
-                :title="$title"
-                href="{{ route('profile', ['username' => $username]) }}"
-                backLabel="Back to profile"
-            />
-            @if ($isCurrentUserProfile)
-                <x-button
-                    x-data
-                    @click="$dispatch('open-modal', 'create-list')"
-                    size="sm"
-                    class="sm:hidden"
-                >
-                    Create list
-                </x-button>
-                <x-button
-                    x-data
-                    @click="$dispatch('open-modal', 'create-list')"
-                    size="md"
-                    class="hidden sm:inline-flex"
-                >
-                    Create list
-                </x-button>
-            @endif
-        </div>
-
-        @if ($lists)
-            <x-section :columns="[2, 'md' => 4, 'lg' => 6]">
-                @foreach ($lists as $list)
-                    <x-list
-                        :title="$list['title']"
-                        :posters="$list['posters']->toArray()"
-                        link="{{ route('list', ['id' => $list['id']]) }}"
-                    />
-                @endforeach
-            </x-section>
+<x-layout class="flex flex-col gap-6 pt-1 md:pt-12">
+    <div class="flex items-start justify-between">
+        <x-section-header.back-link
+            :title="$title"
+            href="{{ route('profile', ['username' => $username]) }}"
+            backLabel="Back to profile"
+        />
+        @if ($isCurrentUserProfile && ! $lists->isEmpty())
+            <x-button
+                x-data
+                @click="$dispatch('open-modal', 'create-list')"
+                size="sm"
+                class="sm:hidden"
+            >
+                Create list
+            </x-button>
+            <x-button
+                x-data
+                @click="$dispatch('open-modal', 'create-list')"
+                size="md"
+                class="hidden sm:inline-flex"
+            >
+                Create list
+            </x-button>
         @endif
     </div>
+
+    @if ($lists->isNotEmpty())
+        <x-section :columns="[2, 'md' => 4, 'lg' => 6]">
+            @foreach ($lists as $list)
+                <x-list
+                    :title="$list['title']"
+                    :posters="$list['posters']->toArray()"
+                    link="{{ route('list', ['id' => $list['id']]) }}"
+                />
+            @endforeach
+        </x-section>
+    @elseif ($lists->isEmpty() && $isCurrentUserProfile)
+        <div class="flex flex-1 flex-col items-center justify-center gap-2">
+            <p class="text-slate-200">You don't have any lists yet!</p>
+            <x-button
+                x-data
+                @click="$dispatch('open-modal', 'create-list')"
+                size="md"
+                class="w-full sm:w-auto"
+            >
+                Create list
+            </x-button>
+        </div>
+    @else
+        <div class="flex flex-1 flex-col items-center justify-center">
+            <p class="text-slate-200">
+                {{ $username }} doesn't have any lists yet!
+            </p>
+        </div>
+    @endif
 
     <x-modal.base
         name="create-list"
