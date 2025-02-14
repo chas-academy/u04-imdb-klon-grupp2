b49afe9c0e9472c556fd45b4e1ffdbb4663eb0a0
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-14 08:23:06 +0100

    feat: render and style users view from admin dashboard

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,3 +1,26 @@
 <x-layout>
-    <h1>All users</h1>
+    <div class="my-4">
+        <x-section-header.back-link
+            backLabel="Back to dashboard"
+            title="All users"
+            href="{{ route('admin.dashboard') }}"
+        />
+    </div>
+
+    <div class="m-auto flex max-w-xl flex-col gap-4">
+        @forelse ($users as $user)
+            <div class="flex items-center justify-between">
+                <x-profile-simplified
+                    :username="'@'. $user->username"
+                    :image="$user->image"
+                    size="md"
+                />
+                <x-button variant="secondary" size="sm" class="ml-auto">
+                    Edit
+                </x-button>
+            </div>
+        @empty
+            <x-empty-state content="No users found." />
+        @endforelse
+    </div>
 </x-layout>
