150525e74f823fb23cfa1170734cce7ea858febb
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-14 21:51:33 +0100

    feat: add all the functions on the ReportController for the report management

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Report;
-use Exception;
+use App\Models\User;
+use App\Models\Review;
 use Illuminate\Http\Request;
+
+use function PHPSTORM_META\map;
 
 class ReportController extends Controller
 {
     /**
+     * 
      * Display a listing of the resource.
      */
     public function index()
@@ -58,6 +62,46 @@ class ReportController extends Controller
     public function show(Report $report)
     {
         //
+    }
+
+    public function reportedReviews()
+    {
+        $reports = Report::with(['review', 'user'])->whereNotNull(['review_id', 'user_id'])->orderBy('updated_at')->get();
+
+        return view('admin.reported-reviews', compact('reports'));
+    }
+
+    public function reportedUsers()
+    {
+        $reports = Report::with(['review', 'user'])->whereNotNull(['user_id'])->whereNull(['review_id'])->orderBy('updated_at')->get();
+
+        $users = $reports->map(function ($report) {
+            return User::where('id', $report->user_id)->first();
+        })->unique();
+
+        return view('admin.reported-users', compact('users'));
+    }
+
+    public function showUserReports($username)
+    {
+        $user = User::where('username', $username)->first();
+        $reports = Report::where('user_id', $user->id)->whereNull(['review_id'])->orderBy('updated_at')->get();
+
+        return view('admin.reported-user', compact('reports', 'username'));
+    }
+
+    public function showReviewReport($id)
+    {
+        $report = Report::with(['review', 'user'])->where('id', $id)->first();
+
+        return view('admin.reported-review', compact('report'));
+    }
+
+    public function clearReport($id)
+    {
+        Report::where('id', $id)->delete();
+
+        return redirect(route('reported.reviews'));
     }
 
     /**
