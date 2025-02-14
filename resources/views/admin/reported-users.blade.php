55fd7793e7549d16bf8a3b2119cf2f4c41966ffa
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-14 21:59:45 +0100

    feat: add styling and rendering for reported users

--- resources/views/admin/reported-users.blade.php
+++ resources/views/admin/reported-users.blade.php
@@ -1,3 +1,31 @@
 <x-layout>
-    <h1>reported users</h1>
+    <div class="my-4">
+        <x-section-header.back-link
+            backLabel="Back to dashboard"
+            title="Reported users"
+            href="{{ route('admin.dashboard') }}"
+        />
+    </div>
+
+    <div class="m-auto flex max-w-xl flex-col gap-4">
+        @forelse ($users as $user)
+            <div class="flex items-center justify-between">
+                <x-profile-simplified
+                    :username="$user->username"
+                    :image="$user->image"
+                    size="md"
+                />
+                <x-button
+                    href="{{ route('reported.user', $user->username) }}"
+                    variant="secondary"
+                    size="sm"
+                    class="ml-auto"
+                >
+                    Check report
+                </x-button>
+            </div>
+        @empty
+            <x-empty-state content="No users found." />
+        @endforelse
+    </div>
 </x-layout>
