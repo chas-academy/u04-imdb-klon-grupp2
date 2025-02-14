fd5ce0a42598bd721ce6f6db1533e5f7a5d688e2
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-15 00:52:14 +0100

    fix: linting errors

--- resources/views/lists.blade.php
+++ resources/views/lists.blade.php
@@ -1,11 +1,8 @@
 <x-layout>
-    @if ($myLists->isEmpty())
-        <div class="mb-6">
-    @else
-        <div class="mt-16 mb-6 flex items-start justify-between">
-    @endif
-
-    @if (Auth::check() && Auth::user()->is($user))
+    <div
+        class="{{ $myLists->isEmpty() ? 'mb-6' : 'mt-16 mb-6 flex items-start justify-between' }}"
+    >
+        @if (Auth::check() && Auth::user()->is($user))
             <x-section-header.back-link
                 title="My lists"
                 backLabel="Back to profile"
@@ -13,14 +10,14 @@
             />
 
             @if ($myLists->isEmpty())
-            <div class="relative flex h-screen items-center justify-center">
-                <div
-                    class="flex -translate-y-1/2 transform flex-col gap-4 text-center"
-                >
-                    <x-empty-state content="You don’t have any lists yet!" />
-            @else
-                    <div class="ml-auto flex items-center">
-            @endif
+                <div class="relative flex h-screen items-center justify-center">
+                    <div
+                        class="flex -translate-y-1/2 transform flex-col gap-4 text-center"
+                    >
+                        <x-empty-state
+                            content="You don’t have any lists yet!"
+                        />
+                    </div>
                     <x-button
                         x-data
                         class="sm:px-2 sm:py-1 md:px-6 md:py-2"
@@ -29,9 +26,18 @@
                         Create list
                     </x-button>
                 </div>
-            </div>
-        </div>
-    @else
+            @else
+                <div class="ml-auto flex items-center">
+                    <x-button
+                        x-data
+                        class="sm:px-2 sm:py-1 md:px-6 md:py-2"
+                        @click="$dispatch('open-modal', 'create-list')"
+                    >
+                        Create list
+                    </x-button>
+                </div>
+            @endif
+        @else
             <x-section-header.back-link
                 title="{{ $user->username }}'s lists"
                 backLabel="Back to profile"
@@ -49,19 +55,20 @@
                     </div>
                 </div>
             @endif
-        </div>
+        @endif
+    </div>
+
+    @if (! $myLists->isEmpty())
+        <x-section :columns="[2, 'sm' => 4, 'lg' => 6]">
+            @foreach ($myLists as $list)
+                <x-list
+                    :title="$list['title']"
+                    :posters="$list['posters']->toArray()"
+                    :link="route('list', ['id' => $list['id']])"
+                />
+            @endforeach
+        </x-section>
     @endif
-
-    <x-section :columns="[2, 'sm' => 4, 'lg' => 6]">
-        @foreach ($myLists as $list)
-            <x-list
-                :title="$list['title']"
-                :posters="$list['posters']->toArray()"
-                :link="route('list', ['id' => $list['id']])"
-            />
-        @endforeach
-    </x-section>
-
 
     <x-create-list-modal />
 </x-layout>
