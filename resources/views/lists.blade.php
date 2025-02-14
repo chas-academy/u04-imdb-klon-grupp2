0ea8ea0101ef2ecbd90e0ee1e0a59ea0a1799261
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-15 00:05:34 +0100

    fix: different views of lists page

--- resources/views/lists.blade.php
+++ resources/views/lists.blade.php
@@ -1,17 +1,26 @@
 <x-layout>
     @if ($myLists->isEmpty())
         <div class="mb-6">
+    @else
+        <div class="mt-16 mb-6 flex items-start justify-between">
+    @endif
+
+    @if (Auth::check() && Auth::user()->is($user))
             <x-section-header.back-link
-                href="{{ route('profile', ['username' => Auth::user()->username]) }}"
-                backLabel="Back to profile"
                 title="My lists"
+                backLabel="Back to profile"
+                href="{{ route('profile', ['username' => Auth::user()->username]) }}"
             />
 
+            @if ($myLists->isEmpty())
             <div class="relative flex h-screen items-center justify-center">
                 <div
                     class="flex -translate-y-1/2 transform flex-col gap-4 text-center"
                 >
                     <x-empty-state content="You don’t have any lists yet!" />
+            @else
+                    <div class="ml-auto flex items-center">
+            @endif
                     <x-button
                         x-data
                         class="sm:px-2 sm:py-1 md:px-6 md:py-2"
@@ -23,35 +32,36 @@
             </div>
         </div>
     @else
-        <div class="mt-16 mb-6 flex items-start justify-between">
             <x-section-header.back-link
-                href="{{ route('profile', ['username' => Auth::user()->username]) }}"
+                title="{{ $user->username }}'s lists"
                 backLabel="Back to profile"
-                title="My lists"
+                href="{{ route('profile', ['username' => $user->username]) }}"
             />
 
-            <div class="ml-auto flex items-center">
-                <x-button
-                    x-data
-                    class="sm:px-2 sm:py-1 md:px-6 md:py-2"
-                    @click="$dispatch('open-modal', 'create-list')"
-                >
-                    Create list
-                </x-button>
-            </div>
+            @if ($myLists->isEmpty())
+                <div class="relative flex h-screen items-center justify-center">
+                    <div
+                        class="flex -translate-y-1/2 transform flex-col gap-4 text-center"
+                    >
+                        <x-empty-state
+                            content="{{ $user->username }} doesn’t have any lists yet!"
+                        />
+                    </div>
+                </div>
+            @endif
         </div>
     @endif
-    @auth
-        <x-section :columns="[2, 'sm' => 4, 'lg' => 6]">
-            @foreach ($myLists as $list)
-                <x-list
-                    :title="$list['title']"
-                    :posters="$list['posters']->toArray()"
-                    :link="route('list', ['id' => $list['id']])"
-                />
-            @endforeach
-        </x-section>
-    @endauth
+
+    <x-section :columns="[2, 'sm' => 4, 'lg' => 6]">
+        @foreach ($myLists as $list)
+            <x-list
+                :title="$list['title']"
+                :posters="$list['posters']->toArray()"
+                :link="route('list', ['id' => $list['id']])"
+            />
+        @endforeach
+    </x-section>
+
 
     <x-create-list-modal />
 </x-layout>
