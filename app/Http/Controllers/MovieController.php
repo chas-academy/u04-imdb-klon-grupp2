390f94a435a3c69f4bb6201822f891dc1acd473c
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-14 19:12:01 +0100

    fix: linting errors

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -28,7 +28,7 @@ class MovieController extends Controller
                     return [
                         'id' => $list->id,
                         'title' => $list->title,
-                        'posters' => $list->movies->map(fn($movie) => [
+                        'posters' => $list->movies->map(fn ($movie) => [
                             'src' => $movie->poster,
                             'title' => $movie->title,
                             'id' => $movie->id,
