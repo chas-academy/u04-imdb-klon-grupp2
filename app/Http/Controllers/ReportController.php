2f492d6138cc251090d95aeca8b2356248a79f17
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-15 03:20:48 +0100

    feat: clear report functions and filter by pending reports (decision_made = false)

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -66,14 +66,18 @@ class ReportController extends Controller
 
     public function reportedReviews()
     {
-        $reports = Report::with(['review', 'user'])->whereNotNull(['review_id', 'user_id'])->orderBy('updated_at')->get();
+        $reports = Report::with(['review'])->whereNotNull(['review_id'])->where('decision_made', false)->latest()->get();
+
+        foreach ($reports as $report) {
+            $report->user = User::where('id', $report->review->user_id)->first();
+        }
 
         return view('admin.reported-reviews', compact('reports'));
     }
 
     public function reportedUsers()
     {
-        $reports = Report::with(['review', 'user'])->whereNotNull(['user_id'])->whereNull(['review_id'])->orderBy('updated_at')->get();
+        $reports = Report::with(['review', 'user'])->whereNotNull(['user_id'])->where('decision_made', false)->orderBy('updated_at')->get();
 
         $users = $reports->map(function ($report) {
             return User::where('id', $report->user_id)->first();
@@ -85,21 +89,45 @@ class ReportController extends Controller
     public function showUserReports($username)
     {
         $user = User::where('username', $username)->first();
-        $reports = Report::where('user_id', $user->id)->whereNull(['review_id'])->orderBy('updated_at')->get();
+        $reports = Report::where('user_id', $user->id)->where('decision_made', false)->orderBy('updated_at')->get();
 
-        return view('admin.reported-user', compact('reports', 'username'));
+        return view('admin.reported-user', compact('reports', 'user'));
     }
 
     public function showReviewReport($id)
     {
-        $report = Report::with(['review', 'user'])->where('id', $id)->first();
+        $report = Report::with(['review'])->where('id', $id)->first();
+        $report->user = User::where('id', $report->review->user_id)->first();
 
         return view('admin.reported-review', compact('report'));
     }
 
-    public function clearReport($id)
+    public function clearUserReport($username, $id)
     {
-        Report::where('id', $id)->delete();
+        $report = Report::where('id', $id)->firstOrFail();
+        $report->decision_made = true;
+        $report->save();
+
+        $user = User::where('username', $username)->firstOrFail();
+        $n_reports = Report::where('user_id', $user->id)->count();
+
+        return $n_reports == 0 ? redirect(route('admin.dashboard')) : redirect(route('reported.user', $username));
+    }
+
+    public function clearReviewReport($id)
+    {
+        $report = Report::where('id', $id)->firstOrFail();
+        $report->decision_made = true;
+        $report->save();
+
+        return redirect(route('reported.reviews'));
+    }
+
+    public function deleteReview($id)
+    {
+        $report = Report::where('id', $id)->firstOrFail();
+        $review = Review::where('id', $report->review_id)->firstOrFail();
+        $review->delete();
 
         return redirect(route('reported.reviews'));
     }
