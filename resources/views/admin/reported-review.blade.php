381b3cdef83487b3ffdf4dda728ed661b7494d97
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-02-15 03:17:19 +0100

    fix: styling of reported review page

--- resources/views/admin/reported-review.blade.php
+++ resources/views/admin/reported-review.blade.php
@@ -15,10 +15,10 @@
                 <div class="ml-auto flex gap-2">
                     <form
                         method="POST"
-                        action="{{ route('clear.report', $report->id) }}"
+                        action="{{ route('clear.review.report', $report->id) }}"
                     >
                         @csrf
-                        @method('DELETE')
+                        @method('PUT')
                         <x-button type="submit" variant="secondary">
                             Clear report
                         </x-button>
@@ -26,7 +26,7 @@
                     <x-button
                         class="bg-red-400"
                         x-data
-                        @click="$dispatch('open-modal', 'ban-user')"
+                        @click="$dispatch('open-modal', 'delete-review')"
                     >
                         Delete review
                     </x-button>
@@ -39,74 +39,60 @@
                     "
                 --}}
             </div>
-            <div class="mt- text-sm text-slate-400">
+
+            <div class="flex max-w-xl flex-col gap-4 text-sm text-slate-400">
+                <x-review
+                    :title="$report->review->movie->title"
+                    :content="$report->review->content"
+                    :image="$report->review->movie->poster"
+                    :rating="$report->review->movie->rating_average"
+                    created_at="{{ $report->review->movie->created_at->diffForHumans() }}"
+                    link="{{ route('movie', ['id' => $report->review->movie->id, 'title' => $report->review->movie->title]) }}"
+                />
+
+                <p>
+                    Reported at:
+                    <span class="text-slate-50">
+                        {{ $report->created_at->diffForHumans() }}
+                    </span>
+                </p>
                 <p>
                     Reported reason:
                     <span class="text-slate-50">{{ "$report->reason" }}</span>
                 </p>
-                <p>
-                    Reported at:
-                    <span class="text-slate-50">
-                        {{-- {{ $report->created_at->diffForHumans() }} --}}
-                    </span>
-                </p>
             </div>
         </div>
-
-        <x-review
-            :title="$report->review->movie->title"
-            :content="$report->review->content"
-            :image="$report->review->movie->poster"
-            :rating="$report->review->movie->rating_average"
-            created_at="{{ $report->review->movie->created_at->diffForHumans() }}"
-            link="{{ route('movie', ['id' => $report->review->movie->id, 'title' => $report->review->movie->title]) }}"
-        />
     </div>
 
-    {{--
-        <x-modal.base
-        name="ban-user"
+    <x-modal.base
+        name="delete-review"
         :show="$errors->banUser->isNotEmpty() || $errors->banUserValidation->isNotEmpty()"
-        >
+    >
         <x-modal.input>
-        <x-slot:title>
-        {{ $report->user->username }}
-        </x-slot>
-        
-        <form
-        method="post"
-        action="{{ route('profile.ban', ['id' => $report->user->id]) }}"
-        class="flex flex-col gap-6"
-        >
-        @csrf
-        @method('put')
-        
-        <div class="flex flex-col gap-4">
-        <x-input.date
-        name="date"
-        :value="old('date')"
-        required
-        :error="$errors->banUserValidation->first('date')"
-        label="Until when should this user be banned?"
-        color="light"
-        />
-        </div>
-        
-        <x-input.error :message="$errors->banUser->first()" />
-        
-        <div class="flex gap-2">
-        <x-button
-        x-data
-        @click="$dispatch('close-modal', 'ban-user')"
-        type="button"
-        variant="secondary"
-        >
-        Cancel
-        </x-button>
-        <x-button class="flex-1">Save</x-button>
-        </div>
-        </form>
+            <x-slot:title>
+                Are you sure you want to delete this review?
+            </x-slot>
+
+            <form
+                method="POST"
+                action="{{ route('delete.review', $report->id) }}"
+                class="flex flex-col gap-6"
+            >
+                @csrf
+                @method('DELETE')
+
+                <div class="flex gap-2">
+                    <x-button
+                        x-data
+                        @click="$dispatch('close-modal', 'delete-review')"
+                        type="button"
+                        variant="secondary"
+                    >
+                        Cancel
+                    </x-button>
+                    <x-button type="submit" class="flex-1">Confirm</x-button>
+                </div>
+            </form>
         </x-modal.input>
-        </x-modal.base>
-    --}}
+    </x-modal.base>
 </x-layout>
